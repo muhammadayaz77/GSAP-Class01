8b30227e1ceb28cc6fd05a8cb4b51df76482793f
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-05-05 23:18:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,161 @@
+// gsap.to('.box',{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate: 360,
+//     backgroundColor: 'yellow',
+//     borderRadius: "50%"
+// })
+
+
+// gsap.to('.box',{
+//     x:1000,
+//     y:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:'yellow',
+//     borderRadius: '50%'
+// })
+
+// gsap.from('h1',{
+//     y:30,
+//     duration:1,
+//     delay:1,
+//     opacity:0,
+//     stagger:0.3
+// })
+
+// gsap.to('.box',{
+//     x:1200,
+//     duration:2,
+//     rotate:360,
+//     delay:0.5,
+//     repeat:-1,
+//     yoyo:true,
+// })
+
+
+// gsap.to('.box1',{
+//     x:1500,
+//     duration:2,
+//     delay:0.5
+// })
+// gsap.to('.box2',{
+//     x:1500,
+//     duration:2,
+//     delay:2.5,
+//     rotate:360,
+// })
+// gsap.to('.box3',{
+//     x:1500,
+//     duration:2,
+//     delay:4.5
+// })
+
+
+// let tl = gsap.timeline();
+
+// tl.to('.box1',{
+//     x:1500,
+//     delay:1,
+//     duration:2,
+// })
+// tl.to('.box2',{
+//     x:1500,
+//     duration:1,
+//     rotate:50
+// })
+// tl.to('.box3',{
+//     x:1500,
+//     duration:2,
+// })
+
+// let tl = gsap.timeline();
+
+// tl.from('h2',{
+//     y:-20,
+//     opacity:0,
+//     duration:1,
+//     delay:0.5,
+
+// })
+// tl.from('h4',{
+//     y:-20,
+//     opacity:0,
+//     delay:0.5,
+//     stagger:0.3,
+// })
+// tl.from('h1',{
+//     y:20,
+//     opacity:0,
+//     duration:0.5,
+//     scale:0.2,
+// })
+// let tl = gsap.timeline();
+// tl.to('.box1',{
+//     x:1100,
+//     duration:2,
+//     delay:1,
+//     // opacity:0,
+//     backgroundColor:'yellow',
+// })
+// tl.to('.box2',{
+//     x:1100,
+//     duration:2,
+//     delay:1,
+//     // opacity:0,
+//     rotate:360,
+//     backgroundColor:'yellow',
+
+// })
+// tl.to('.box3',{
+//     x:1100,
+//     duration:2,
+//     delay:1,
+//     // opacity:0,
+//     backgroundColor:'yellow',
+// })
+// let tl = gsap.timeline()
+// tl.from('h2',{
+//     y:-20,
+//     opacity:0,
+//     duration:1,
+//     delay:0.5
+// })
+// tl.from('h4',{
+//     y:-20,
+//     opacity:0,
+//     duration:1,
+//     delay:0.5,
+//     stagger:0.3,
+// })
+// tl.from('h1',{
+//     y:-10,
+//     opacity:0,
+//     duration:1,
+//     delay:0.5,
+//     scale:0.5
+// })
+
+// let tl = gsap.timeline();
+// tl.from('h2',{
+//     y:-20,
+//     duration:0.6,
+//     opacity:0,
+// })
+
+// tl.from('h4',{
+//     y:-20,
+//     opacity:0,
+//     duration:0.6,
+//     stagger:0.3,
+// })
+// tl.from('h1',{
+//     y:-10,
+//     opacity:0,
+//     duration:0.6,
+//     scale:0.5,
+//     rotate:720
+    
+// })
